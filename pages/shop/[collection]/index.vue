45ede4a16fa262e99cc4f4c2aca165e1624f9569
bootsmo95 <ab@bootsmann.dk>
2026-04-20 18:56:37 +0200

    fix: single product fills full width

--- pages/shop/[collection]/index.vue
+++ pages/shop/[collection]/index.vue
@@ -60,7 +60,7 @@ watch(data, (newData) => {
         <p v-if="collection.description" class="max-w-2xl text-lg leading-8 text-stone-600">{{ collection.description }}</p>
       </div>
 
-      <div v-if="products.length > 0" class="grid gap-5 md:grid-cols-2">
+      <div v-if="products.length > 0" :class="['grid gap-5', products.length === 1 ? 'grid-cols-1' : 'md:grid-cols-2']">
         <NuxtLink
           v-for="product in products"
           :key="product.id"
